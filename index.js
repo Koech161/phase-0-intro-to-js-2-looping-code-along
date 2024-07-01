9d6ec8f137173905fc6f4f92694c2acca7bdbbac
Abel Koech <koechabel161@gmail.com>
2024-07-01 19:37:35 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names= ("Guadalupe", "Ollie", "Aki")
+const event ="suprise"
+function writeCards(names, event){
+    let thankYouMessage=[]
+    for(let i=0;i<names.length;i++){
+        let message= `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thankYouMessage.push(message)
+    }return thankYouMessage
+}
+let i =10
+function countDown(number){
+    for(i=number;i>=0;i--){
+        console.log(i)
+    }
+}
